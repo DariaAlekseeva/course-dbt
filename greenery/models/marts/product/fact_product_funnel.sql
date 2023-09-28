9a3809a1d89bc6b818244bb70bef51b5bf2b8b6c
Daria Alekseeva <alekseevadd@gmail.com>
2023-09-28 19:57:59 +0000

    Week 3 project

--- greenery/models/marts/product/fact_product_funnel.sql
+++ greenery/models/marts/product/fact_product_funnel.sql
@@ -8,9 +8,6 @@
 select 
 date(created_at)                                                  as date
 , user_id
-, max(case when event_type = 'page_view' then 1 else 0 end)       as is_page_view
-, max(case when event_type = 'add_to_cart' then 1 else 0 end)     as is_add_to_cart
-, max(case when event_type = 'checkout' then 1 else 0 end)        as is_checkout
-, max(case when event_type = 'package_shipped' then 1 else 0 end) as is_package_shipped
+{{ event_processor('stg_postgres__events', 'event_type') }}
 from {{ ref ('stg_postgres__events') }}
 group by all
